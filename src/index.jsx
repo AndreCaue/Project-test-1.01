fa6bc3ee0052b1549b15ce0f387b3c1e3af6ca90
Andre Caue <andrecaue@outlook.com>
2023-04-19 00:28:19 -0300

    project-1.01 finish

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './templates/App';
+import Home from './templates/App/index';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,18 +1,97 @@
-import { Posts } from '../../components/Posts';
-import { CounterProvider } from '../../contexts/CounterProvider';
-import { PostsProvider } from '../../contexts/PostsProvider';
-import './styles.css';
+import { useEffect, useRef, useState } from "react";
 
-function App() {
-  return (
-    <CounterProvider>
-      <PostsProvider>
-        <div>
-          <Posts />
+const isObjectEqual = (ObjA, ObjB) => {
+  return JSON.stringify(ObjA) === JSON.stringify(ObjB);
+};
+
+const useFetch = (url, options) => {
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [shouldLoad, setShouldLoad] = useState(false);
+  const urlRef = useRef(url);
+  const optionsRef = useRef(options);
+
+  useEffect(() => {
+    let changed = false;
+
+    if (!isObjectEqual(url, urlRef.current)) {
+      urlRef.current = url;
+      changed = true;
+    }
+    if (!isObjectEqual(options, optionsRef.current)) {
+      optionsRef.current = options;
+      changed = true;
+    }
+
+    if (changed) {
+      setShouldLoad((s) => !s)
+    }
+  }, [url, options])
+
+  useEffect(() => {
+    let wait = false;
+    setLoading(true)
+
+    const fetchData = async () => {
+      await new Promise(r => setTimeout(r, 1000));
+
+      try {
+        const response = await fetch(urlRef.current, optionsRef.current);
+        const jsonResult = await response.json();
+        if (!wait) {
+          setResult(jsonResult);
+          setLoading(false);
+        }
+      } catch (e) {
+        if (!wait) {
+          setLoading(false);
+        }
+        throw e;
+      }
+    }
+    fetchData();
+
+    return () => {
+      wait = true;
+    }
+  }, [shouldLoad])
+
+  return [result, loading];
+
+};
+const Home = () => {
+  const [postId, setPostId] = useState('');
+  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts/' + postId,
+    { headers: { abc: '1' + postId } });
+
+  useEffect(() => {
+    console.log('ID DO POST', postId)
+  }, [postId])
+  if (loading) {
+    return <p>Loading....</p>
+  }
+  const handleClick = (id) => {
+    setPostId(id);
+  }
+
+  if (!loading && result) {
+    return <div>
+      {result?.length > 0 ? result.map(p => (<div key={`post-${p.id}`} onClick={() => handleClick(p.id)}>
+        <p>{p.title}</p>
+      </div>)) : (
+        <div onClick={() => handleClick('')}>
+          <p>{result.title}</p>
         </div>
-      </PostsProvider>
-    </CounterProvider>
+      )}
+
+    </div>
+  }
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
   )
 }
 
-export default App;
+export default Home;
